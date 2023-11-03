dbe36eadaf8987dc20975236e69f4110aa3edb56
GitHub Action <action@github.com>
2023-11-03 06:42:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/dyr11/imageveiw/MyBundle.kt
+++ src/main/kotlin/com/github/dyr11/imageveiw/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dyr11.imageveiw
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/dyr11/imageveiw/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/dyr11/imageveiw/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.dyr11.imageveiw.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dyr11.imageveiw.MyBundle
+import com.github.dyr11.imageveiw.services.MyProjectService
 import javax.swing.JButton
 
 
